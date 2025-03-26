49377dd1178b0620932e5bb533b26c612b104442
NIraj Balpande <nirajbalpande12@gmail.com>
2025-03-26 14:48:26 +0530

    Sorted file

--- coreJava/src/coreJava/A1_Encapsulation.java
+++ coreJava/src/coreJava/A1_Encapsulation.java
@@ -1,6 +1,6 @@
 package coreJava;
-
-public class Encapsulation {
+//Wrapping of data in single unit is encapsulation
+public class A1_Encapsulation {
 
 	public static void main(String[] args) {
 		Employee e=new Employee();
